61bd85c6867f957cbcb90bc5e9d91b68518d84b4
Maryam5676 <maryamnasir2942004@gmail.com>
2024-09-17 17:13:54 +0500

    add comment to each section

--- node/index.mjs
+++ node/index.mjs
@@ -2,16 +2,19 @@
 import { createServer } from 'node:http'
 
 const myServer = createServer((req, res) => {
-    
+    // Homepage
     if (req.url === '/') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('This is the Home Page.');
+        // About
     } else if (req.url === '/about') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('This is the About Page.');
+        //Contact
     }else if (req.url === '/Contact') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('This is the Contact Page.');
+        //404page
     }
     else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
