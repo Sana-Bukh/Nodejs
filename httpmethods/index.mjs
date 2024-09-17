127da7eff3455b395e9401a3657f3d5ed6e2dc90
Maryam5676 <maryamnasir2942004@gmail.com>
2024-09-17 17:53:00 +0500

    add comments to section

--- httpmethods/index.mjs
+++ httpmethods/index.mjs
@@ -36,15 +36,25 @@ const server = createServer((req, res) => {
         const itemId = parsedUrl.pathname.split('/').pop();
         req.on('data', chunk => {
           body += chunk.toString();
-        }); // DELETE Request
-    } else if (method === 'DELETE' && parsedUrl.pathname.startsWith('/api/items/')) {
-        const itemId = parsedUrl.pathname.split('/').pop();
-        res.statusCode = 200;
-        res.end(JSON.stringify({ message: `DELETE request - Deleting item ${itemId}` }));
-        
-        // Handle 404 Not Found
-    } else {
-        res.statusCode = 404;
-        res.end(JSON.stringify({ message: 'Route not found' }));
-    }
+        });
+        req.on('end', () => {
+          const updatedItem = JSON.parse(body);
+          res.statusCode = 200;
+          res.end(JSON.stringify({ message: `PUT request - Updating item ${itemId}`, data: updatedItem }));
+        });
     
+        // DELETE Request
+        } else if (method === 'DELETE' && parsedUrl.pathname.startsWith('/api/items/')) {
+            const itemId = parsedUrl.pathname.split('/').pop();
+            res.statusCode = 200;
+            res.end(JSON.stringify({ message: `DELETE request - Deleting item ${itemId}` }));
+    
+        // Handle 404 Not Found
+        } else {
+            res.statusCode = 404;
+            res.end(JSON.stringify({ message: 'Route not found' }));
+        }
+        
+});
+
+server.listen(PORT, () => console.log(`Server is listening here: http://localhost:${PORT}`))
