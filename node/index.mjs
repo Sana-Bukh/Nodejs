710d611ecab89e595ec9e9377106e4bd2dc39876
Maryam5676 <maryamnasir2942004@gmail.com>
2024-09-17 16:49:55 +0500

    HomeRoute

---
+++ node/index.mjs
@@ -0,0 +1,31 @@
+
+import { createServer } from 'node:http'
+
+const myServer = createServer((req, res) => {
+    
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('This is the Home Page.');
+    } else if (req.url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('This is the About Page.');
+    }else if (req.url === '/Contact') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('This is the Contact Page.');
+    }
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+    }
+})
+
+myServer.listen(200, () => {
+    console.log( "server is listening at 200 port" )
+})
+
+
+
+
+
+
+ 
